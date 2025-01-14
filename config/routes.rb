ca576d93d75da119aa4104cd246abfbd117fc6db
Henrique Motta <henriqueamotta@gmail.com>
2025-01-14 20:08:44 -0300

    devise/user implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   resources :ingredients, only: [:index, :new, :create]
   resources :recipes, only: [:index, :show]
   root to: "pages#home"
