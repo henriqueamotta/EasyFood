efd1dde33c7da95e32e37dec7bb110eb0b4d3bb8
Beatriz Zonaro <77759777+bezoc@users.noreply.github.com>
2025-01-29 19:21:29 -0300

    fix user login and registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
 
   def home
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
