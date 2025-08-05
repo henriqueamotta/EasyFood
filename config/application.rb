96e96149204bcd4a281b013be62610cdba476aae
Henrique Motta <henriqueamotta@gmail.com>
2025-08-05 18:44:00 -0300

    feat: Initialise new version of EasyFood project

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,8 @@ Bundler.require(*Rails.groups)
 
 module EasyFood
   class Application < Rails::Application
-    config.action_controller.raise_on_missing_callback_actions = false if Rails.version >= "7.1.0"
-    config.generators do |generate|
-      generate.assets false
-      generate.helper false
-      generate.test_framework :test_unit, fixture: false
-    end
-
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
