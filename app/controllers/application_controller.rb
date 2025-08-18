30be82d591dfae161870081dfddd892de6c81cf8
Henrique Motta <henriqueamotta@gmail.com>
2025-08-18 18:20:45 -0300

    i18n on footer + navbar implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern # Permitir apenas navegadores modernos
+  before_action :set_locale # Definir o locale antes de cada ação
   before_action :configure_permitted_parameters, if: :devise_controller? # Permitir parâmetros adicionais para o Devise
+
+  private
+
+  def set_locale # Definir o locale com base nos parâmetros, sessão ou padrão
+    locale = params[:locale] || session[:locale] || I18n.default_locale # Definir o locale
+
+    if params[:locale].present? && I18n.available_locales.map(&:to_s).include?(params[:locale]) # Verificar se o locale é válido
+      session[:locale] = params[:locale] # Armazenar o locale na sessão
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale # Definir o locale
+  end
 
   protected
 
